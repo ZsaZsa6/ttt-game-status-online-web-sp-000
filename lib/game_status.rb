291cf9a3c16cfaf851feee74e61b29d774a888bf
Ronda D Cowen <cowenhome5@gmail.com>
2019-09-24 20:42:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,50 @@
-# Helper Method
+require "pry"
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+WIN_COMBINATIONS = [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 4, 8], 
+  [2, 4, 6], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8]
+  ]
+  
+def won?(board)
+ 
+  
+  WIN_COMBINATIONS.each do |win_combo| 
+    index_1 = win_combo[0]
+    index_2 = win_combo[1]
+    index_3 = win_combo[2]
+    
+    
+    element_1 = board[index_1]
+    element_2 = board[index_2]
+    element_3 = board[index_3]
+    
+    if element_1 == "X" && element_2 == "X" && element_3 == "X" || element_1 == "O" && element_2 == "O" && element_3 == "O"
+      return win_combo
+   end
+  end
+  board.any? == "X" || board.any? == "O"
+end
+  
+  # binding.pry
+  
+  
+def full?(board)
+  board.all? {|element|element == "X" || element == "O"}
+  
+end
 
-# Define your WIN_COMBINATIONS constant
+def draw?(board)
+won?(board) && full?(board) || won?(board) || full?(board)
+end
+
+# def over?(board)`
+#   board == won?(board) || board == draw?(board)
